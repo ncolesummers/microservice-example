b3d8cd8172d96c8ac0594d6b3f86fc271e35f49b
Nate Summers <nsummers72@gmail.com>
2020-10-31 13:02:51 -0700

    Fix main.go

--- src/eventsservice/main.go
+++ src/eventsservice/main.go
@@ -1,11 +1,23 @@
 package eventsservice
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"github.com/ncolesummers/microservice-example/src/eventsservice/rest"
+	"github.com/ncolesummers/microservice-example/src/lib/configuration"
+	"github.com/ncolesummers/microservice-example/src/lib/persistence/dblayer"
+	"log"
+)
 
 func main() {
 	confPath := flag.String("conf", `.\configuration\config.json`, "flag to set the path to the configuration json file")
 	flag.Parse()
 	//extract configuration
-	config, _ := configurati
+	config, _ := configuration.ExtractConfiguration(*confPath)
+	fmt.Println("Connecting to database")
+	dbhandler, _ := dblayer.NewPersistenceLayer(config.Databasetype, config.DBConnection)
+
+	// RESTful API start
+	log.Fatal(rest.ServeAPI(config.RestfulEndpoint, dbhandler))
 }
 
